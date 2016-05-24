a15f1bdc6a89f6beff5e4492e4d52d3fd9b12670
Rafael Frank <rafael.frank@sap.com>
2016-05-23 22:01:59 -0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<link rel="stylesheet" href="assets/lib.css">
+</head>
+<body>
+	<?php
+		$count = 0;
+
+		if(array_key_exists('count',$_GET)){
+			$count = $_GET['count'];
+		}
+
+		for ($x = 0; $x < $count; $x++) {
+			echo "<div>NA</div>";
+		}
+	?>
+	<div>BATMAN!!!</div>
+</body>
+</html>
